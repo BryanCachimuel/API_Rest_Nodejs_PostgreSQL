6f28abd56303a8cf09707627fbf912ae5c84f6a0
BryanCachimuel <bryanloyo56@gmail.com>
2022-07-13 20:19:42 -0500

    configurando la exportación de los modelos hacia la base de datos mediante sequelize

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import app from "./app.js";
 import { sequelize } from "./database/database.js";
 
+import './models/Project.js'
+
+/*
+     await sequelize.sync({force:true}) -> esto permite que en base al modelo creado
+     se exporte y se combierta en una entidad en la base de datos.
+     y lo que esta dentro de las llaves hace que el modelo se importe de nuevo
+     si se le hace algun cambio
+*/
+
 async function main() {
   try {
     await sequelize.authenticate();
     console.log("Conexión hacia la base de datos exitosa");
+    await sequelize.sync({force:true})
     app.listen(3004);
     console.log("Servidor en el puerto 3004");
   } catch (error) {
--- src/models/Project.js
+++ src/models/Project.js
@@ -1,5 +1,5 @@
 import { DataTypes } from 'sequelize'
-import { sequelize } from '../database/database'
+import { sequelize } from '../database/database.js'
 
 export const Projects = sequelize.define('projects',{
     id:{
@@ -15,6 +15,8 @@ export const Projects = sequelize.define('projects',{
     },
     description:{
         type: DataTypes.STRING
-    }
-})
+    },
+}, {
+    timestamps: true
+});
 
