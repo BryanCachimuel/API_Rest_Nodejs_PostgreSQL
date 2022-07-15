6cc014795eed6c145a296973a6e49dcbccfb16a2
BryanCachimuel <bryanloyo56@gmail.com>
2022-07-14 19:57:05 -0500

    creando las rutas con las que se utilizarán las tareas

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,6 +1,10 @@
 import { Router } from 'express'
 const router = Router()
 
-
+router.get('/tasks')
+router.post('/tasks')
+router.put('/tasks/:id')
+router.delete('/tasks/:id')
+router.get('/tasks/:id')
 
 export default router
