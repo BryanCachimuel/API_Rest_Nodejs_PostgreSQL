43880904df393cd51e3c49559ad1308e58e2ea53
BryanCachimuel <bryanloyo56@gmail.com>
2022-07-14 20:47:47 -0500

    importando los métodos actualizar, eliminar y extraer tareas hacia las rutas de tareas

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express'
-import { createTasks, getTasks } from '../controllers/tasks.controller.js'
+import { createTasks, deleteTask, getTask, getTasks, updateTask } from '../controllers/tasks.controller.js'
 const router = Router()
 
 router.get('/tareas', getTasks)
 router.post('/tareas/crear', createTasks)
-router.put('/tareas/:id')
-router.delete('/tareas/:id')
-router.get('/tareas/:id')
+router.put('/tareas/:id',updateTask)
+router.delete('/tareas/:id', deleteTask)
+router.get('/tareas/:id', getTask)
 
 export default router
