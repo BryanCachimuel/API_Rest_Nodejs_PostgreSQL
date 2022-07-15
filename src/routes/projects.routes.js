789a1b43cbdae115add05ccc00af1e5e1cb09d2c
BryanCachimuel <bryanloyo56@gmail.com>
2022-07-14 19:25:23 -0500

    agregando método para borrar un proyecto

--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express'
-import { createProject, getProjects } from '../controllers/projects.controller.js'
+import { createProject, deleteProject, getProjects, updateProject } from '../controllers/projects.controller.js'
 
 const router = Router()
 
 router.get('/proyectos', getProjects); 
 router.post('/proyectos/crear', createProject);
-router.put('/proyectos/:id');
-router.delete('/proyectos/:id');
+router.put('/proyectos/:id', updateProject);
+router.delete('/proyectos/:id', deleteProject);
 router.get('/proyectos/:id');
 
 export default router
